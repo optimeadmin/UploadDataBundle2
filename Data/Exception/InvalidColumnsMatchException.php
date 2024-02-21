44131685ea4d8b901a504c34198271fcf0b7e708
maguirre <maguirre@optimeconsulting.com>
2024-02-21 09:53:24 -0500

    Agregando toValidationErrors al InvalidColumnsMatchException

--- Data/Exception/InvalidColumnsMatchException.php
+++ Data/Exception/InvalidColumnsMatchException.php
@@ -7,6 +7,9 @@ namespace Manuel\Bundle\UploadDataBundle\Data\Exception;
 
 use Exception;
 use Symfony\Component\Translation\TranslatableMessage;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
@@ -35,5 +38,20 @@ class InvalidColumnsMatchException extends Exception
         }
 
         return ['message' => $this->translatableMessage->trans($translator)];
+    }
+
+    public function toValidationErrors(
+        string $propertyPath,
+        TranslatorInterface $translator = null
+    ): ConstraintViolationListInterface {
+        if (!$this->translatableMessage || !$translator) {
+            $message = $this->message;
+        } else {
+            $message = $this->translatableMessage->trans($translator);
+        }
+
+        return new ConstraintViolationList([
+            new ConstraintViolation($message, null, [], null, $propertyPath, null),
+        ]);
     }
 }
